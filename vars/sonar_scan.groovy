1a7032265c652953117d4b03e21faf3cb0e29707
vishesh <visheshg5050@gmail.com>
2025-05-09 15:52:44 +0530

    Update sonar_scan.groovy

--- vars/sonar_scan.groovy
+++ vars/sonar_scan.groovy
@@ -5,7 +5,7 @@ def call(String scannerHome, String sonarInstanceName, String projectKey, String
               -Dsonar.projectKey=${projectKey} \
               -Dsonar.projectName=${projectName} \
               -Dsonar.projectVersion=${projectVersion} \
-              -Dsonar.sources=${sources}
+              -Dsonar.sources=${sources} \
               -Dsonar.java.binaries=customer-service/target/classes,order-service/target/classes,config-server/target/classes,eureka-server/target/classes,zuul-server/target/classes,oauth2-server/target/classes,admin-server/target/classes
         """
     }
